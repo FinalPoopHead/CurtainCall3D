cf1d147350abc392199e7a7636e7bbb734676d67
kkangddol <knemo333@gmail.com>
2024-06-12 14:19:29 +0900

    SpriteRenderer 의 position의 z값을 depth로 사용하게 했음.

--- RocketEngine/RocketDX11/SpriteRenderer.cpp
+++ RocketEngine/RocketDX11/SpriteRenderer.cpp
@@ -48,13 +48,16 @@ namespace Rocket::Core
 		_worldTM.Decompose(scale, quat, pos);
 
 		spriteBatch->Draw(
-			_texture->GetSRV(),
-			DirectX::XMFLOAT2(pos.x, pos.y),
-			nullptr,
-			_color,
-			0.0f,										//회전 각도
-			DirectX::XMFLOAT2(0.5f, 0.5f),				//  이미지의 원점->0.0f,0.0f이면 좌측상단
-			DirectX::XMFLOAT2(scale.x, scale.y));		// 이미지 스케일
+			_texture->GetSRV()
+			, DirectX::XMFLOAT2(pos.x, pos.y)
+			, nullptr
+			, _color
+			, 0.0f										//회전 각도
+			, DirectX::XMFLOAT2(0.5f, 0.5f)				//  이미지의 원점->0.0f,0.0f이면 좌측상단
+			, DirectX::XMFLOAT2(scale.x, scale.y)		// 이미지 스케일
+			, DirectX::SpriteEffects::SpriteEffects_None
+			, pos.z										// 이미지 depth
+		);		
 	}
 
 	void SpriteRenderer::SetColor(Color color)
