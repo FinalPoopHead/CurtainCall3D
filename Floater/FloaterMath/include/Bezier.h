55afa10ba21826ea0aa33f8c78a97c67cde9ca00
DESKTOP-I724423\kocca61 <cgyk.piro@gmail.com>
2024-06-17 15:39:01 +0900

    텍스트 변경 시 재 등록하지 않아 변경된 텍스트가 적용되지 않는 버그 수정 베지어 곡선관련 함수 구현중.

---
+++ Floater/FloaterMath/include/Bezier.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include "Vector2f.h"
+#include <array>
+#include <vector>
+#include <map>
+
+
+namespace flt
+{
+	class  Bezier
+	{
+	public:
+		__declspec(dllexport) Bezier();
+		__declspec(dllexport) ~Bezier() {}
+
+		__declspec(dllexport) Vector2f Evaluate(float t) const;
+		__declspec(dllexport) void AddControlPoint(const Vector2f& controlPoint);
+
+		__declspec(dllexport) float operator()(float y) const;
+
+	private:
+		static std::array<std::array<float, 16>, 16> s_binomialCoefficients;
+		static float binomialCoefficient(int n, int k);
+
+		float CalcX(float t) const;
+		float CalcY(float t) const;
+		float CalcYPrime(float t) const;
+
+		std::vector<Vector2f> _controlPoints;
+	};
+
+	//class QuadraticBezier : public Bezier
+	//{
+	//public:
+
+	//private:
+	//};
+
+	//class CubicBezier : public Bezier
+	//{
+	//public:
+
+	//private:
+	//};
+}
+
+
