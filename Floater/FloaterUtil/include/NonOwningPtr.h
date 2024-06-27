decb738e583ee02991fbd05a79dd7ad503d50cb0
DESKTOP-I724423\kocca61 <cgyk.piro@gmail.com>
2024-06-27 16:21:48 +0900

    게임 로직 lifeTime 변경 -> enable 후 바로 disable처리로 동시에 일어나면 반드시 비활성화, update는 한번도 물리지 않음. 포인터 리턴하는 api의 경우 잘못 사용하는 것을 막기 위한 래핑 클래스 구현 펄린 노이즈 구현을 위한 파일 생성. 에러코드 래핑용 구현을 위한 클래스 생성 로그 기능 추가 카메라 흔들기 위한 클래스 추가.

---
+++ Floater/FloaterUtil/include/NonOwningPtr.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "OwningPtr.h"
+
+namespace flt
+{
+	template<typename T>
+	class NonOwningPtr
+	{
+	public:
+		constexpr NonOwningPtr() noexcept : NonOwningPtr(nullptr) { }
+		constexpr NonOwningPtr(T* ptr) noexcept : _ptr(ptr) { }
+		constexpr NonOwningPtr(const NonOwningPtr& other) noexcept : _ptr(other._ptr) { }
+		constexpr NonOwningPtr(OwningPtr<T>& other) noexcept : _ptr(other.get()) { }
+		constexpr NonOwningPtr(const OwningPtr<T>& other) noexcept : _ptr(other.get()) { }
+
+		NonOwningPtr(NonOwningPtr&& other) = delete;
+
+		constexpr NonOwningPtr& operator=(const NonOwningPtr& other) noexcept { _ptr = other._ptr; return *this; }
+		constexpr NonOwningPtr& operator=(const OwningPtr<T>& other) noexcept { _ptr = other.get(); return *this; }
+		~NonOwningPtr() noexcept {}
+
+		constexpr [[nodiscard]] T* get() const noexcept { return _ptr; }
+		constexpr [[nodiscard]] T* operator->() const { return _ptr; }
+		constexpr [[nodiscard]] T& operator*() const { return *_ptr; }
+
+	private:
+		T* _ptr;
+	};
+}
