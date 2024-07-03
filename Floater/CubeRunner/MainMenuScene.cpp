036b6353b3d1bd59244e547a562fd62ba752e43e
DESKTOP-I724423\kocca61 <cgyk.piro@gmail.com>
2024-07-03 12:19:32 +0900

    메인 메뉴의 item 변경

--- Floater/CubeRunner/MainMenuScene.cpp
+++ Floater/CubeRunner/MainMenuScene.cpp
@@ -11,37 +11,43 @@ void MainMenuScene::Initialize()
 {
 	Menu* menu = flt::CreateGameObject<Menu>(true);
 
+	flt::Vector2f menuItemSize = { 164.0f, 66.4f };
+	menuItemSize *= 2.0f;
+	float menuItemOffsetX = 0.85f;
+	float menuItemOffsetY = 0.65f;
+
 	MenuItem* item1 = flt::CreateGameObject<MenuItem>(true);
 	menu->AddItem(item1);
 
-	item1->SetSprite(L"../Resources/Sprites/abcd.jpg");
+	item1->SetSprite(L"../Resources/Sprites/SinglePlayer2.png");
 	//item1->SetPosition({ 1000.0f, 400.0f });
-	item1->SetOffsetPosition({ 0.75f, 0.65f });
-	item1->SetSize({100.0f, 80.0f});
+	item1->SetOffsetPosition({ menuItemOffsetX, menuItemOffsetY });
+	menuItemOffsetY += 0.1f;
+	item1->SetSize(menuItemSize);
 
-	item1->SetSelectFunc([]() { flt::SetScene(L"class GameScene"); g_PlayerNum = 2; g_StageNum = 1; });
+	item1->SetSelectFunc([]() { g_PlayerNum = 1; g_StageNum = 1; flt::SetScene(L"class GameScene"); });
 
 
 	MenuItem* item2 = flt::CreateGameObject<MenuItem>(true);
 	menu->AddItem(item2);
 
-	item2->SetSprite(L"../Resources/Sprites/test.jpg");
+	item2->SetSprite(L"../Resources/Sprites/MultiPlayer2.png");
 	//item2->SetPosition({ 1000.0f, 500.0f });
-	item2->SetOffsetPosition({ 0.75f, 0.8f });
-	item2->SetSize({ 100.0f, 80.0f });
+	item2->SetOffsetPosition({ menuItemOffsetX, menuItemOffsetY });
+	menuItemOffsetY += 0.1f;
+	item2->SetSize(menuItemSize);
 
-	item2->SetSelectFunc([]() { flt::ExitGame(); });
+	item2->SetSelectFunc([]() { g_PlayerNum = 2; g_StageNum = 1; flt::SetScene(L"class GameScene"); });
 
 	MenuItem* item3 = flt::CreateGameObject<MenuItem>(true);
 	menu->AddItem(item3);
 
-	item3->SetText(L"Test");
-	item3->SetFont(L"../Resources/Fonts/NotoSansKR.spritefont");
-	item3->SetTextColor(1.0f, 0.0f, 0.0f);
-	item3->SetOffsetPosition({ 0.75f, 0.5f });
+	item3->SetSprite(L"../Resources/Sprites/Exit.png");
+	item3->SetOffsetPosition({ menuItemOffsetX, menuItemOffsetY });
+	menuItemOffsetY += 0.1f;
+	item3->SetSize(menuItemSize);
 
-	item3->SetSelectFunc([]() { flt::SetScene(L"class GameScene"); g_PlayerNum = 1; g_StageNum = 1; });
-
+	item3->SetSelectFunc([]() { flt::ExitGame(); });
 
 	MenuSelector* selector = flt::CreateGameObject<MenuSelector>(true, menu);
 
