2f6e12e4e03a9a299d7a520d4d1ef5f6bd91d934
DESKTOP-I724423\kocca61 <cgyk.piro@gmail.com>
2024-07-12 16:59:49 +0900

    디폴트 easing함수 구현중

---
+++ Floater/FloaterMath/include/Ease.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "Bezier.h"
+
+
+namespace flt
+{
+	namespace Ease
+	{
+		const Bezier& bezierLinear = Bezier::Linear();
+	}
+	//class Ease
+	//{
+	//public:
+	//	float operator()(float t) const
+	//	{
+	//		return ease(t);
+	//	}
+
+	//private:
+	//	float (*ease)(float t);
+	//};
+
+	//class Linear : public Ease
+	//{
+	//public:
+	//	Linear() : Ease()
+	//	{
+	//		ease = [](float t) -> float
+	//			{
+	//			return t;
+	//		};
+	//	}
+	//};
+}
