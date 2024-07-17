67247a4cef66812370afff5e3d48ccb294811f03
kkangddol <knemo333@gmail.com>
2024-07-17 14:41:20 +0900

    garbage line drop할때 Line 째로 드랍하지않고 빈 공간이 존재할 수 있도록 드랍하도록 변경함.

--- Floater/CubeRunner/Board.cpp
+++ Floater/CubeRunner/Board.cpp
@@ -596,9 +596,11 @@ void Board::DropGarbageLine(int lineCount)
 	float heightDelay = 0.15f;
 	float randomDelayValue = 0.08f;
 
+	int dropChance = 50;
+
 	int darkCube = 7;
 	int advantageCube = 15;
-	int normalCube = 78;
+	int normalCube = 100 - darkCube - advantageCube;
 
 	std::random_device rd;
 
@@ -606,6 +608,12 @@ void Board::DropGarbageLine(int lineCount)
 	{
 		for (int i = 0; i < width; ++i)
 		{
+			int dropValue = rd() % 100;
+			if (dropValue >= dropChance)
+			{
+				continue;
+			}
+
 			float x = _tiles[i][j]->tr.GetWorldPosition().x;
 			float z = _tiles[i][j]->tr.GetWorldPosition().z;
 
