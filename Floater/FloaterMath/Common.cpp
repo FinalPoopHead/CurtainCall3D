e8ba87eb46d1ab92e953dbc7363fda4c775cb5b8
DESKTOP-I724423\kocca61 <cgyk.piro@gmail.com>
2024-07-17 11:52:07 +0900

    smoothStep 함수 구현 shake 임시 구현을 위한 함수 추가

--- Floater/FloaterMath/Common.cpp
+++ Floater/FloaterMath/Common.cpp
@@ -17,4 +17,35 @@ namespace flt
 	//float FLOAT_EPSILON = 1.192092896e-07F; 
 	// 1.0f == 1.0 + FLOAT_EPSILON 이 false가 나오는 가장 작은 값.
 	float FLOAT_EPSILON = std::numeric_limits<float>::epsilon();
-}
+
+	float SmoothStep(float t)
+	{
+		if (t == 0.0f)
+		{
+			return 0.0f;
+		}
+		else if (t == 1.0f)
+		{
+			return 1.0f;
+		}
+		else
+		{
+			return t * t * (3.0f - 2.0f * t);
+		}
+	}
+
+	float MoreSmoothStep(float t)
+	{
+		if (t == 0.0f)
+		{
+			return 0.0f;
+		}
+		else if (t == 1.0f)
+		{
+			return 1.0f;
+		}
+		else
+		{
+			return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f);
+		}
+	}
