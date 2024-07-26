3f58a7c8d28f65c5cb0249ef0b05ad83e32a7127
DESKTOP-I724423\kocca61 <cgyk.piro@gmail.com>
2024-07-26 11:47:42 +0900

    콘을 위 아래로 흔들리게

---
+++ Floater/CubeRunner/MineCone.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "../FloaterGameEngine/include/EngineMinimal.h"
+
+class MineCone : public flt::GameObject
+{
+public:
+	MineCone(GameObject* parentMine, const std::wstring& texturePath);
+
+protected:
+	virtual void Update(float deltaTime) override;
+	virtual void OnEnable() override;
+private:
+	float _moveSpeed;
+	float _acceleration;
+	float _coneHeight;
+};
